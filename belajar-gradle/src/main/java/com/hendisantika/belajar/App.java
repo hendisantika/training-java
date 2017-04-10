5b0fe45a2eec3b8c5773a60bf743cacbb3c65aff
Hendi Santika <hendisantika@gmail.com>
2017-04-10 18:00:58 +0700

    #13 - Added More files

--- belajar-gradle/src/main/java/com/hendisantika/belajar/App.java
+++ belajar-gradle/src/main/java/com/hendisantika/belajar/App.java
@@ -2,6 +2,12 @@ package com.hendisantika.belajar;
 
 /**
  * Created by hendisantika on 4/10/17.
+ *
+ * cara 1 : gradle -PmainClass=com.hendisantika.belajar.App execute
+ * cara 2 : gradle run
  */
 public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
