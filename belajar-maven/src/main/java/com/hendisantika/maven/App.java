a5fffcb60e09d34fc08d39f56a94a83ca1c17411
Hendi Santika <hendisantika@gmail.com>
2017-04-11 20:16:20 +0700

    #14 - Fix maven exec

---
+++ belajar-maven/src/main/java/com/hendisantika/maven/App.java
@@ -0,0 +1,15 @@
+package com.hendisantika.maven;
+
+/**
+ * Created by hendisantika on 4/11/17.
+ * mvn compile
+ * mvn exec:java -Dexec.mainClass="com.hendisantika.maven.App"
+ * mvn clean install exec:java
+
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Mari belajar Java dengan Maven");
+    }
+}
