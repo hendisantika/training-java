96042501afc9f2dee251a807ae304d7e0b1504c4
Hendi Santika <hendisantika@gmail.com>
2017-04-13 07:01:36 +0700

    #15 - Fixed Bug gradle

--- belajar-gradle/src/main/java/com/hendisantika/belajar/App.java
+++ belajar-gradle/src/main/java/com/hendisantika/belajar/App.java
@@ -2,12 +2,14 @@ package com.hendisantika.belajar;
 
 /**
  * Created by hendisantika on 4/10/17.
- *
+ * <p>
  * cara 1 : gradle -PmainClass=com.hendisantika.belajar.App execute
  * cara 2 : gradle run
  */
 public class App {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        System.out.println("Mari belajar Java via Gradle!");
     }
 }
