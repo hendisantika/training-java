6ae2f498d5f9b181ed142124f15f775e1e84ed49
Hendi Santika <hendisantika@gmail.com>
2017-04-20 17:24:19 +0700

    #34 - Add Domain / Entity Class

---
+++ aplikasi-pelatihan/src/main/java/com/transkom/pelatihan/entity/Materi.java
@@ -0,0 +1,64 @@
+package com.transkom.pelatihan.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity @Table(name = "materi")
+public class Materi {
+    
+    @Id @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    
+    @Column(nullable = false, unique = true, length = 10)
+    private String kode;
+    
+    @Column(nullable = false)
+    private String nama;
+    
+    @JsonIgnore
+    @OneToMany(
+        cascade = CascadeType.ALL, 
+        orphanRemoval = true,
+        mappedBy = "materi"
+    )
+    private List<Sesi> daftarSesi = new ArrayList<>();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public List<Sesi> getDaftarSesi() {
+        return daftarSesi;
+    }
+
+    public void setDaftarSesi(List<Sesi> daftarSesi) {
+        this.daftarSesi = daftarSesi;
+    }
+    
+    
+}
